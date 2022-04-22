d246d7c83a527e3eb854fc6a0540b19f0fa1da6f
Gianluca Jahn <gianluca.jahn98@gmail.com>
2022-04-22 19:52:54 +0200

    Edit terms in profitOrLoss array

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Creating Arrays for the assignRandomValues function to randomly choose an index from.
 const overviews = ["Insane", "Wow", "Great", "Oh dear", "Too bad", "Oh no! Damn... that's actually horrible"];
 const assetClasses = ["Stocks", "leveraged Options", "ETF", "Real Estate properties","Bitcoins", "NFTs", "Pokemon Cards", "toilet paper"];
-const profitOrLoss = ["rose", "grew", "surged", "dropped", "dumped", "crashed"];
-const callToAction = ["buy more right now", "wait for the next surge to sell", "sell them for the great profit", "hold them some longer", "cut losses quickly", "buy the dip"];
+const profitOrLoss = ["rose", "grew", "surged", "dropped", "dumped", "plummeled"];
+const callToAction = ["buy more right now", "wait for the next surge to sell", "sell them for a great profit", "hold them some longer", "cut losses quickly", "buy the dip"];
 // Calculating a random number between 0 (inclusive) and 100 (exclusive) to generate a randomized profit or loss percentage between 0% and 99%.
 const randomPercentage = Math.floor(Math.random() * 100);
 
